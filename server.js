5e9625b1d48c1daeab3743315e0c8f2929d8f11d
CaseySMiller <CaseySMiller@yahoo.com>
2024-04-22 20:49:40 -0600

    unlinked homeroutes and put a static path to public in server.js. Also added style.js an image and script.js

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
+
 
 
 sequelize.sync({ force: false }).then(() => {
